9861a862e7eedfabe6b3657240acd0341641885e
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2023-12-09 17:42:44 -0300

    Atualização do patch.
    Co-authored-by: Raphael Azambuja <RaphaelAzambuja@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-// import { axios } from "axios";
-
-
-// configurações do front end
-
 const modal = document.querySelector('.modal-container');
 const tbody = document.querySelector('tbody');
 const sNome = document.querySelector('#m-nome');
 const sFuncao = document.querySelector('#m-funcao');
 const sSalario = document.querySelector('#m-salario');
-const btnSalvar = document.querySelector('#btnSalvar');
+let btnSalvar = document.querySelector('#btnSalvar');
 let itens;
 let id;
+
+let btnEditar = document.querySelectorAll(".btn-editar");
+
+document.addEventListener("click", function() 
+{
+  openModal(); // abrir o modal
+  const id = event.target.id; //pegar id do botão editar apertado
+  btnSalvar.id = "btnAtualizar"; //mudar id do btnSalvar
+  
+  atualizarDados(id)
+  //Continuar essa parte
+  
+})
+
+function atualizarDados(id) //função para atualizar os dados salvos
+{
+  axios.patch(`http://localhost:3006/teste/${id}`)
+  .then(response => 
+  {
+    console.log(response)
+    alert ('Operação efetuada com sucesso.')
+    
+    .catch(error => 
+    {
+      console.error('Erro ao atualizar usuário', error);
+    });
+  })
+}
 
 window.onload = () => {
   criarGet();
@@ -31,14 +54,12 @@ btnSalvar.onclick = e => {
 
   e.preventDefault();
 
-  
   const itens = { 
     'nome': sNome.value, 
     'funcao': sFuncao.value, 
     'salario': sSalario.value 
   }
   
-
   setItensBD();
 
   modal.classList.remove('active');
@@ -93,14 +114,28 @@ function chamaGet (response)
   response.data.forEach(item => {
     const tr = document.createElement('tr');
     tr.innerHTML = `
-      <td>${item.nome}</td>
-      <td>${item.funcao}</td>
-      <td>${item.salario}</td>
+      <td id="nome">${item.nome}</td>
+      <td id="funcao">${item.funcao}</td>
+      <td id="salario">${item.salario}</td>
+      <td><button id="${item.id}" class="action-button btn-editar">editar</button></td>
+      <td><button id="${item.id}" class="action-button btn-excluir">Excluir</button></td>
     `;
     tbody.appendChild(tr);
   });
 
 }
+
+function editItem()
+{
+ btnSalvar.onclickS
+ 
+  //chamar o btn-editar e abrir o modal atraves da função openmodal()
+  //pegar o valor do id do botão apertado rota/da/funçaõ/valorid
+  //  const idbtn = valor do id
+  //  axios.put(`http://localhost:3006/teste/${idbtn}`) exemplo de uso 
+     
+}
+
 
 function alertaPost (response) {
   console.log (response)
