6824157d8c355c3e86144bf717dfaf876d3b0310
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2023-12-08 21:12:50 -0300

    Criação do método get.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // import { axios } from "axios";
 
+
+
+// configurações do front end
+
 const modal = document.querySelector('.modal-container');
 const tbody = document.querySelector('tbody');
 const sNome = document.querySelector('#m-nome');
@@ -9,12 +13,17 @@ const btnSalvar = document.querySelector('#btnSalvar');
 let itens;
 let id;
 
+window.onload = () => {
+  criarGet();
+}
+
 function openModal() {
   modal.classList.add('active');
   sNome.value = '';
   sFuncao.value = '';
   sSalario.value = '';
 }
+
 
 btnSalvar.onclick = e => {
   if (sNome.value == '' || sFuncao.value == '' || sSalario.value == '') {
@@ -34,49 +43,46 @@ btnSalvar.onclick = e => {
   setItensBD();
 
   modal.classList.remove('active');
-  // loadItens();
   id = undefined;
 
   axios.post('http://localhost:3006/teste', itens)
     .then(response => {
       console.log(response);
+      criarGet();
+
     })
     .catch(error => {
       console.error('Erro ao adicionar usuário', error);
     });
 };
 
-// function loadItens() {
-//   itens = getItensBD()
-//   tbody.innerHTML = ''
-//   itens.forEach((item, index) => {
-//     insertItem(item, index)
-//   })
-
-// }
 
 const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
 
-// loadItens()
+
+function criarGet () {
+axios.get('http://localhost:3006/teste')
+.then(response => {
+// Limpe o conteúdo atual da tabela
+        tbody.innerHTML = '';
+
+      // Adicione os novos dados à tabela
+      response.data.forEach(item => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+          <td>${item.nome}</td>
+          <td>${item.funcao}</td>
+          <td>${item.salario}</td>
+        `;
+        tbody.appendChild(tr);
+      });
 
 
+console.log(response);
 
-function validarUsuarioSenha() {
-  // Verificar se o usuário e a senha correspondem aos valores desejados
-  var usuario = document.getElementById("username").value;
-  var senha = document.getElementById("password").value;
-
-  return usuario === "lucasbeckh@live.com" && senha === "123";
-}
-
-function redirecionarParaOutraPagina() {
-  // Verificar se as credenciais são válidas
-  if (validarUsuarioSenha()) {
-    window.location.href = "main.html";
-  } else {
-
-    var mensagemErro = document.getElementById("mensagem-erro");
-    mensagemErro.innerText = "Credenciais inválidas. Por favor, tente novamente.";
-  }
-}
+})
+.catch(error => {
+  console.error('Erro ao efetuar o get');
+});
+};
