f19a6badb8fefd82374719e99b2d889f3eb0470b
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2023-12-15 02:54:20 -0300

    finalização

--- script.js
+++ script.js
@@ -1,53 +1,113 @@
-const empresaForm = document.getElementById('empresaForm');
-const doacaoForm = document.getElementById('doacaoForm');
-const produtoForm = document.getElementById('produtoForm');
-
 window.onload = () => {
-    //criarGet();
-}
+  const colunasUsuarios = ['nome', 'cnpj', 'endereco', 'telefone', 'email', 'tipo', 'id'];
+  const colunasDoacoes = ['tipo', 'quantidade', 'disponibilidade', 'validade', 'id'];
+  const colunasProdutos = ['nome', 'id'];
 
+  carregarTabela('usuario', 'tabelaUsuarios', colunasUsuarios);
+  carregarTabela('doacao', 'tabelaDoacoes', colunasDoacoes);
+  carregarTabela('produto', 'tabelaProdutos', colunasProdutos);
 
+  const usuarioForm = document.getElementById('usuarioForm');
+  usuarioForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    cadastrar(this, 'usuario');
+  });
+
+  const doacaoForm = document.getElementById('doacaoForm');
+  doacaoForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    cadastrar(this, 'doacao');
+  });
+
+  const produtoForm = document.getElementById('produtoForm');
+  produtoForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    cadastrar(this, 'produto');
+  });
+
+  const excluirForm = document.getElementById('excluirForm');
+  excluirForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const tipo = this.tipoExcluir.value;
+    const id = this.idExcluir.value;
+
+    if (!tipo || !id) {
+      alert('Por favor, selecione o tipo e insira o ID.');
+      return;
+    }
+
+    const confirmacao = confirm('Tem certeza de que deseja excluir este registro?');
+
+    if (confirmacao) {
+      excluirRegistro(tipo, id);
+    }
+  });
+};
 
 function alertaPost(response) {
-    
-    if (response.status >= 200 && response.status < 300) {
-        alert('Post realizado com sucesso!');
-    } else {
-        
-        alert('Erro ao realizar o post. Verifique o console para mais detalhes.');
-        console.error(response);
-    }
+  if (response.status >= 200 && response.status < 300) {
+    alert('Operação realizada com sucesso!');
+    recarregarPagina();
+  } else {
+    alert('Erro na operação. Verifique o console para mais detalhes.');
+    console.error(response);
+  }
 }
 
-function cadastrar(form, endpoint) 
-{
-    const formData = new FormData(form);
+function cadastrar(form, endpoint) {
+  const formData = new FormData(form);
+  
+  axios.post(`http://localhost:8080/${endpoint}`, Object.fromEntries(formData))
+    .then(response => {
+      alertaPost(response);
+    })
+    .catch(error => {
+      console.error(`Erro ao realizar operação ${endpoint}:`, error);
+    });
+}
 
-    axios.post(`http://localhost:8080/${endpoint}`, Object.fromEntries(formData))
-        .then(response => {
-            alertaPost(response);
-            //criarGet();
-        })
-        .catch(error => {
-            console.error('Erro ao cadastrar:', error);
+function carregarTabela(endpoint, tabelaId, colunas) {
+  axios.get(`http://localhost:8080/${endpoint}`)
+    .then(response => {
+      const tabela = document.getElementById(tabelaId);
+      const tbody = tabela.querySelector('tbody');
+      tbody.innerHTML = "";
+
+      response.data.forEach(item => {
+        const row = document.createElement("tr");
+
+        colunas.forEach(coluna => {
+          const cell = document.createElement("td");
+
+          if (coluna === "id") {
+            row.setAttribute("data-id", item[coluna]);
+          }
+
+          cell.textContent = item[coluna];
+          row.appendChild(cell);
         });
+
+        tbody.appendChild(row);
+      });
+    })
+    .catch(error => {
+      console.error(`Erro ao carregar tabela de ${endpoint}:`, error);
+    });
 }
 
-empresaForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    cadastrar(this, 'usuario'); 
-});
 
-doacaoForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    cadastrar(this, 'doacao'); 
-});
+//AXIOS DELETE
+function excluirRegistro(tipo, id) {
+  axios.delete(`http://localhost:8080/${tipo}/${id}`)
+    .then(response => {
+      alertaPost(response);
+    })
+    .catch(error => {
+      console.error(`Erro ao excluir registro de ${tipo}:`, error);
+      recarregarPagina()
+    });
+}
 
-produtoForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    cadastrar(this, 'produto'); 
-});
-
-//function criarGet() {
-    
-//}
+function recarregarPagina() {
+  location.reload();
+}
